e53a8ac9e9db6dc1954a792322a18c57a7b5b9d2
JP Agustin <jepoy.agustin@gmail.com>
2023-11-05 08:45:17 +0800

    unit test implementation

--- JPA-WebApiApp/Program.cs
+++ JPA-WebApiApp/Program.cs
@@ -1,35 +1,46 @@
-var builder = WebApplication.CreateBuilder(args);
+using JPA_WebApiApp.Service;
+using JPA_WebApiApp.Service.Interface;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
+internal class Program
 {
-    options.AddDefaultPolicy(builder =>
+    public static async Task Main(string[] args)
     {
-        builder.WithOrigins("http://localhost:4200"); // Update with your frontend URL
-    });
-});
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        // Add services to the container.
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder.WithOrigins("http://localhost:4200"); // Update with your frontend URL
+            });
+        });
+
+        builder.Services.AddSingleton<IEncoderService, EncoderService>();
+
+        var app = builder.Build();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseCors();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
